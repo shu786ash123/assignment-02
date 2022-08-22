8e896ca5f64db101f41e723c51ed4c76a99ae428
ASHISH SHUKLA <106545357+shu786ash123@users.noreply.github.com>
2022-08-23 01:05:17 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    float a;
+    printf("enter inr:  ");
+    scanf("%d",&x);
+    a = x * (1/76.23);
+    printf(" usd conersion is %.2f",a);
+    return 0;
+}
